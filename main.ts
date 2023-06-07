9838f63470e54fb0e7668cd3b62dfc42e3340983
Egyed József <jegyed50@gmail.com>
2023-06-07 09:46:00 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,38 @@
 function texttobackward (originaltext: string) {
     texttoreturn = ""
     originaltextlength = originaltext.length
-    for (let currentcharpositionindex = 0; currentcharpositionindex <= originaltextlength; currentcharpositionindex++) {
-        texttoreturn = "" + texttoreturn + originaltext.charAt(originaltextlength - currentcharpositionindex)
+    for (let texttobackwardindex1 = 0; texttobackwardindex1 <= originaltextlength; texttobackwardindex1++) {
+        currentcharpositionindex = originaltextlength - texttobackwardindex1
+        current_char = originaltext.charAt(currentcharpositionindex)
+        texttoreturn = "" + texttoreturn + current_char
     }
     return texttoreturn
 }
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (texttobackward(text1)))
+})
+function showtextwithoutscroll (text: string, pausebetweencharacters: number, clearscreenbetweenchars: boolean) {
+    for (let showtextwithoutscrollindex1 = 0; showtextwithoutscrollindex1 <= "Hello".length; showtextwithoutscrollindex1++) {
+        basic.showString(text.charAt(showtextwithoutscrollindex1))
+        if (clearscreenbetweenchars) {
+            basic.pause(pausebetweencharacters / 2)
+            basic.clearScreen()
+            basic.pause(pausebetweencharacters / 2)
+        } else {
+            basic.pause(pausebetweencharacters)
+        }
+    }
+    return 0
+}
+input.onButtonPressed(Button.B, function () {
+    showtextwithoutscroll("ESZTER", 100, true)
+})
+let current_char = ""
+let currentcharpositionindex = 0
 let originaltextlength = 0
 let texttoreturn = ""
-let text1 = "Picur1"
-basic.showString("" + (texttobackward(text1)))
+let text1 = ""
+text1 = "Picur"
 basic.forever(function () {
 	
 })
